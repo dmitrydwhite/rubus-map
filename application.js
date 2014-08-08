98ed36ddf1eb2d85e8571d8248eaa5568a953af4
Dmitry White <white.dmitry@gmail.com>
2014-08-08 16:43:06 -0700

    Added some listeners for client events, some styling.

--- application.js
+++ application.js
@@ -9,32 +9,47 @@ var app = (function() {
     reDraw();
   });
 
+  // Helper function for Navbar Listener
+  var reDraw = function() {
+    if (navClass === 'navbutton picking') {
+      $('.content').remove();
+      $('.navbar').after(rubusTemplate);
+      getLocation();
+      sharePatch();
+    }
+    else {
+      $('.content').remove();
+      $('.navbar').after('<div class="content"></div>');
+      $('.content').append('<div class="clear"></div>');
+      $('.content').append($('<p>', {
+        'class': 'error_msg',
+        text: 'We\'re sorry, this function is still ' +
+          'under construction. Please check back soon!'
+      }));
+    }
+  };
+
   // Set Listeners and Methods for "Picking" tab
 
   // Listener for toggling whether to use device
   // location for submit.
   var getLocation = function() {
     $('.toggler').click(function() {
-      if ($('.loc_switch').text() === 'OFF') {
-        $('.loc_switch').text('ON');
+      if ($('.loc_switch').text() === 'ENTER MANUALLY') {
+        $('.loc_switch').text('USE GPS');
       } else {
-        $('.loc_switch').text('OFF');
+        $('.loc_switch').text('ENTER MANUALLY');
       }
     });
-  }
-
-  var reDraw = function() {
-    if (navClass === 'navbutton picking') {
-      $('.content').remove();
-      $('.navbar').after(rubusTemplate);
-      getLocation();
-    }
-    else {
-      $('.content').remove();
-      $('.navbar').after('<div class="content"></div>');
-      $('.content').append('<div class="clear"></div>');
-      $('.content').append('<p>This No Worky!</p>');
-    }
   };
+
+  // Listener for "Share Berry Patch" button
+  var sharePatch = function() {
+    $('.button').click(function() {
+      // TODO: Gather all data and submit to a database
+      alert('You are Fixing to Share Your Berries!');
+    });
+  };
+
 
 })();
--- rubus.js
+++ rubus.js
@@ -1,2 +1,14 @@
+'use strict';
+
 var rubusTemplate =
-  '<div class="content"><div class="button">Berries!</div><div class="toggler">Toggle whether or not to use Current Device Location</div><div class="loc_switch">OFF</div><p class="input_text">Type in your location</p><input type="text"><p>How Many?</p><input type="range"><p>How Ripe?</p><input type="range"></div>';
+  '<div class="content">' +
+  '<div class="button">Share This Berry Patch</div>' +
+  '<div class="toggler">Use Current Device Location?' +
+  '<div class="loc_switch">ENTER MANUALLY</div></div>' +
+  '<p class="input_text">Type in your location</p>' +
+  '<input type="text">' +
+  '<p>How Many?</p>' +
+  '<input type="range">' +
+  '<p>How Ripe?</p>' +
+  '<input type="range">' +
+  '</div>';
