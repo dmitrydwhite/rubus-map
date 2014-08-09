4786763d39326a75e9f3d2c48cf37f5c4f9c7195
Dmitry White <white.dmitry@gmail.com>
2014-08-09 12:34:51 -0700

    Used express server, updated file structure.

---
+++ public/scripts/application.js
@@ -0,0 +1,83 @@
+'use strict';
+
+var app = (function() {
+  var navClass = 'navbutton picking';
+  var lastLoc;
+
+  // Set an event listener for navbar buttons
+  $('.navbutton').click(function() {
+    $('.navbutton').removeClass('nav_on');
+    navClass = $(this).attr('class');
+    $(this).addClass('nav_on');
+    reDraw();
+  });
+
+  // Helper function for Navbar Listener
+  var reDraw = function() {
+    if (navClass === 'navbutton picking') {
+      $('.content').remove();
+      $('.navbar').after(rubusTemplate);
+      getLocation();
+      sharePatch();
+    }
+    else {
+    // TODO: Create templates for "jamming" and "finding"
+      $('.content').remove();
+      $('.navbar').after('<div class="content"></div>');
+      $('.content').append('<div class="clear"></div>');
+      $('.content').append($('<p>', {
+        'class': 'error_msg',
+        text: 'We\'re sorry, this function is still ' +
+          'under construction. Please check back soon!'
+      }));
+    }
+  };
+
+  // Set Listeners and Methods for "Picking" tab
+  // TODO: Set Listeners and methods for other two tabs
+
+  // Listener for toggling whether to use device
+  // location for submit.
+  var getLocation = function() {
+    $('.toggler').click(function() {
+      if ($('.loc_switch').text() === 'ENTER MANUALLY') {
+        $('.loc_switch').text('USING GPS');
+        findDevice();
+      } else {
+        $('.where').val('');
+        $('.where').attr('placeholder', 'Type an address or coordinates');
+        $('.loc_switch').text('ENTER MANUALLY');
+      }
+    });
+  };
+
+  // Function to find device's location
+  var findDevice = function() {
+    $('.where').attr('placeholder', 'Using device GPS location...')
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var latString = position.coords.latitude.toString().slice(0,6);
+      var longString = position.coords.longitude.toString().slice(0,7);
+      var positionString = latString + ', ' + longString;
+      lastLoc = positionString;
+      $('.where').val(lastLoc);
+    });
+  };
+
+  // Listener for "Share Berry Patch" button
+  var sharePatch = function() {
+    $('.button').click(function() {
+      var patchData = {};
+      patchData.loc = $('.where').val();
+      // TODO: Check for location type, convert to GPS
+      // coordinates as necessary before storing
+      patchData.fecundity = $('.how_many').val();
+      patchData.maturity = $('.how_ripe').val();
+      patchData.nature = 'pick';
+      // TODO: Add timestamp for data decay
+      // TODO: Submit gathered data to a database
+      console.log(patchData);
+      $.ajax('http://localhost:3000/api/patch', {data: patchData});
+    });
+  };
+
+})();
--- server.js
+++ server.js
@@ -1,15 +1,98 @@
-'use strict';
-
+var _ = require('lodash');
 var express = require('express');
-var server = express();
 
-var router = express.Router();
+var env = process.env.NODE_ENV || 'development';
+var knexConfig = require('./knexfile.js')[env];
+var knex = require('knex')(knexConfig);
+var bookshelf = require('bookshelf')(knex);
+var bluebird = require('bluebird'), Promise = bluebird;
+var pg = bluebird.promisifyAll(require('pg'));
 
-router.post('patch', function(req, res) {
-  console.log('connected to server, made post request');
-  console.log(req);
-});
 
-server.use('/api', router);
 
-server.listen(3000);
+var createApp = module.exports.app = function (options, client) {
+  var app = express();
+  app.use(require('morgan')(env === 'development' ? 'dev' : 'default'));
+  app.use(require('body-parser')());
+  app.use(require('method-override')());
+  app.use(express.static(__dirname + '/public'));
+
+  var People = bookshelf.Model.extend({
+    tableName: 'people'
+  });
+
+  app.get('/', function (req, res) {
+    console.log('achieving redirect');
+    res.redirect('/home/');
+  });
+
+  app.get('/api/people', function (req, res) {
+    People.fetchAll().then(function(result) {
+      res.json({people: result.toJSON()});
+    })
+    .done();
+  });
+
+  app.get('/api/people/:id', function (req, res) {
+    People.where({id: req.params.id}).fetchAll().then(function(result) {
+      res.json({person: result.toJSON()});
+    })
+    .done();
+  });
+
+  app.post('/api/people', function (req, res) {
+    People.forge(req.body)
+      .save().then(function(result) {
+        res.json({created: result.toJSON()});
+      })
+      .done();
+  });
+
+  app.put('/api/people/:id', function (req, res) {
+    console.log('attempting to delete');
+    console.log(req.body);
+    People.where({id: req.params.id}).fetch().then(function(person) {
+      return person.save(req.body);
+    }).then(function(person) {
+      res.json({updated: person.toJSON()});
+    })
+    .done();
+  });
+
+  app.delete('/api/people/:id', function (req, res) {
+    var destroyedPerson;
+    People.where({id: req.params.id}).fetch().then(function(person) {
+      destroyedPerson = person.clone();
+      return person.destroy();
+    }).then(function() {
+      res.json({destroyed: destroyedPerson.toJSON()});
+    })
+    .done();
+  });
+  return app;
+};
+
+console.log('loading server resource');
+
+if (require.main === module) {
+  var settings = {
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 3000,
+    dbURL: process.env.DATABASE_URL ||
+      'postgres://localhost/jsi_express'
+  };
+
+  pg.connectAsync(settings.dbURL).spread(function(client, done) {
+    createApp(settings, bluebird.promisifyAll(client))
+    .listen(settings.port, function() {
+      console.log('Express server started on port %s', settings.port);
+    });
+  })
+  .catch(function(e) {
+    console.log(e);
+    console.error('Could not connect to database: %s', settings.dbURL);
+    process.exit(1);
+  });
+}
+
+
